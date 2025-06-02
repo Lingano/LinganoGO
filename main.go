a0bbbc709e498585e0f2e78e6d4be02eec845bd1
CuPidev <mateusz.cupryniak@usi.ch>
2025-06-02 18:00:53 +0200

    fix: Change server port from 8080 to 8081 in main.go

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func main() {
 	authenticatedAPIRouter.HandleFunc("/saved-words/{savedWordID}", handlers.DeleteSavedWord).Methods("DELETE")
 
 	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	if err := http.ListenAndServe(":8081", r); err != nil {
 		log.Fatal(err)
 	}
 }
