52b6309db0af4cbf21fecc1a276f1a313283428e
Mateusz Cupryniak <mateusz.cupryniak@usi.ch>
2025-07-18 23:49:33 +0200

    adding UpdateFlashcard to the flashcard_service

--- services/flashcard_service.go
+++ services/flashcard_service.go
@@ -3,6 +3,10 @@ package services
 import (
 	"LinganoGO/config"
 	"LinganoGO/ent"
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
 )
 
 type FlashcardService struct {
@@ -13,4 +17,21 @@ func NewFlashcardService() *FlashcardService {
 	return &FlashcardService{
 		client: config.GetEntClient(),
 	}
+}
+
+func (s *FlashcardService) UpdateFlashcard(ctx context.Context, id string, question string, answer string) (*ent.Flashcard, error) {
+	flashcardUUID, err := uuid.Parse(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid flashcard ID: %w", err)
+	}
+	flashcard, err := s.client.Flashcard.
+		UpdateOneID(flashcardUUID).
+		SetQuestion(question).
+		SetAnswer(answer).
+		Save(ctx)
+		
+	if err != nil {
+		return nil, fmt.Errorf("failed to update flashcard: %w", err)
+	}
+	return flashcard, nil
 }
